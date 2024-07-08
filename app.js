823bc91b616ec7cbe9d9a2082debff7c988a7916
Manas Mohanty <manasmohanty07@gmail.com>
2024-07-08 14:23:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,164 @@
-const map = L.map('mapid').setView([37.8, -96], 4); // Center of the US
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 10,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
-
 // Object to keep track of selected states
 const selectedStates = {};
+const selectedFeatures = new Map();
+$('document').ready(() => {
 
-// Load GeoJSON from an external file
-$.getJSON("us-states.json", function(geoJsonData) {
-    L.geoJson(geoJsonData, {
-        style: function (feature) {
-            return {color: 'blue', weight: 2};
-        },
-        onEachFeature: function (feature, layer) {
-            // Add state names on the map
-            const bounds = layer.getBounds();
-            const center = bounds.getCenter();
-            L.marker(center, {
-                icon: L.divIcon({
-                    className: 'state-label',
-                    html: feature.properties.name,
-                    iconSize: [100, 40]
-                })
-            }).addTo(map);
-
-            // Click handler for each state
-            layer.on('click', function () {
-                const stateName = feature.properties.name;
-                // Toggle selection
-                if (selectedStates[stateName]) {
-                    delete selectedStates[stateName];
-                    layer.setStyle({color: 'blue'});
-                } else {
-                    selectedStates[stateName] = true;
-                    layer.setStyle({color: 'red'});
-                }
-                console.log(feature);
-                updateSidebar();
-            });
+ 
+    var map;
+    const renderMap = () => {
+        
+        if(map){
+            map.off();
+            map.remove();
         }
-    }).addTo(map);
+        map = L.map('mapid').setView([37.8, -96], 4); // Center of the US
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 10,
+            attribution: '© OpenStreetMap'
+        }).addTo(map);
+        $.getJSON("us-states.json", function (geoJsonData) {
+            //$("#selected-account").val("account1");
+            if ($.isEmptyObject(selectedStates)) {
+                $('#submit').prop('disabled', true);
+            }
+            L.geoJson(geoJsonData, {
+                style: function (feature) {
+                    return { color: 'blue', weight: 2 };
+                },
+                onEachFeature: function (feature, layer) {
+                    // Add state names on the map
+                    const bounds = layer.getBounds();
+                    const center = bounds.getCenter();
+                    L.marker(center, {
+                        icon: L.divIcon({
+                            className: 'state-label',
+                            html: feature.properties.name,
+                            iconSize: [100, 40]
+                        })
+                    }).addTo(map);
+                    const currentStateName = feature.properties.name;
+                    if(selectedStates && selectedStates[currentStateName]){
+                        layer.setStyle({ color: 'red' });
+                        $('#submit').prop('disabled', false);
+                    }
+                    // Click handler for each state
+                    layer.on('click', function () {
+                        const stateName = feature.properties.name;
+                        const featureId = feature.id;
+                        // Toggle selection
+                        if (selectedStates[stateName]) {
+                            delete selectedStates[stateName];
+                            delete selectedFeatures[featureId];
+                            layer.setStyle({ color: 'blue' });
+                        } else {
+                            selectedStates[stateName] = true;
+                            selectedFeatures[featureId] = stateName;
+                            layer.setStyle({ color: 'red' });
+                        }
+                        console.log(feature);
+                        updateSidebar();
+                        if ($.isEmptyObject(selectedStates)) {
+                            $('#submit').prop('disabled', true);
+                        } else {
+                            $('#submit').prop('disabled', false);
+                        }
+                    });
+                }
+            }).addTo(map);
+        });
+    }
+
+    // Load GeoJSON from an external file
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://mocki.io/v1/58d26a31-df69-4909-9d23-b5a29bdd9e3c',
+            success: function (data) {
+                console.log(data);
+                // Populate account dropdown
+                const accountList = data.map(account => `<option value="${account.accountId}">${account.accountName}</option>`);
+                $('#selected-account').append(accountList);
+            }
+        });
+        renderMap();
+
+
+
+    function updateSidebar() {
+        const sidebar = document.getElementById('selected-states');
+        sidebar.innerHTML = '';
+        for (const state in selectedStates) {
+            sidebar.innerHTML += `<div class="state-name">${state}</div>`;
+        }
+    }
+    function submit() {
+        const account = $('#selected-account').val();
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8090/states',
+            data: {
+                data: JSON.stringify({ 'account_number': account,'state_map' : selectedFeatures })
+            },
+            success: function (data) {
+                console.log(data);
+            }
+        })
+    };
+
+    $('#selected-account').on('change', function (e) {
+        const account = $(this).val(e.target.value);
+        var item=$(this);
+        var accountNumber = item.val();
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8090/states/{accountNumber}',
+            success: function (data) {
+                console.log(data);
+                // Populate selected states
+                // Create an array of selected states
+                var s = data['states'];
+                for (var i = 0; i < s.length; i++) {
+                    selectedStates[s[i].state_name] = true;
+                    selectedFeatures[s[i].id] = s[i].state_name;
+                }
+
+                updateSidebar();
+                renderMap();
+            }
+        });
+
+        
+    });
+
 });
 
-function updateSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.innerHTML = 'Selected States:<br>';
-    for (const state in selectedStates) {
-        sidebar.innerHTML += `<div class="state-name">${state}</div>`;
+function submit() {
+    const account = $('#selected-account').val();
+    const mapData = new Array();
+    for (const [key, value] of Object.entries(selectedFeatures)) {
+        //console.log(key, value);
+        //mapData['id'] = key;
+        //mapData['state_name'] = value;
+        mapData.push(key);
     }
-}
+    var finalData = new Map();
+    finalData['account_number'] = account;
+    finalData['state_ids'] = mapData;
+    var json = JSON.stringify(finalData);
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8090/states',
+        headers: { 
+            'Content-Type': 'application/json' 
+          },
+        crossDomain: true, 
+        data: {
+            data: json
+        },
+        success: function (data) {
+            console.log(data);
+        }
+    })
+};
